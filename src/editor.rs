1cc8b1b75a94b03f5f7507455e0632bb2665090a
sohaib farsi <ssohaibfarsi@pm.me>
2023-07-03 23:12:14 +0200

    implemented a 'clear screen' functionality when launching the program, and made it more robust against errors

--- src/editor.rs
+++ src/editor.rs
@@ -1,40 +1,64 @@
-use std::io::{self, stdout};
+use std::io::{self, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-pub struct Editor {}
+pub struct Editor {
+    should_quit: bool,
+}
 
 impl Editor {
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         let _stdout = stdout().into_raw_mode().unwrap();
 
-        for key in io::stdin().keys() {
-            match key {
-                Ok(key) => match key {
-                    Key::Char(c) => {
-                        if c.is_control() {
-                            println!("{:?}\r", c as u8);
-                        } else {
-                            println!("{:?} ({})\r", c as u8, c);
-                        }
-                    }
+        // loop
+        loop {
+            // if let is the same as match but only for one case
+            if let Err(error) = self.refresh_screen() {
+                die(error);
+            }
+            if self.should_quit {
+                break;
+            }
 
-                    Key::Ctrl('q') => break,
-                
-                    _ => println!("{:?}\r", key),
-                }
-
-                Err(err) => die(err),
+            if let Err(error) = self.process_keypress() {
+                die(error);
             }
         }
     }
-
+    // default function for Editor
     pub fn default() -> Self {
-        Self {}
+        Self { should_quit: false }
     }
+
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+        let pressed_key = read_key()?;
+        match pressed_key {
+            Key::Ctrl('q') => self.should_quit = true,
+            _ => (),
+        }
+        Ok(())
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        // without termion: 
+        // print!("\x1b[2J");
+
+        // with termion:
+        print!("{}", termion::clear::All);
+        io::stdout().flush()    
+    }
+
 }
 
 fn die(e: std::io::Error) {
     panic!("{}", e);
+}
+
+fn read_key() -> Result<Key, std::io::Error> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #![warn(clippy::all, clippy::pedantic)]
 mod editor;
+
+
 use editor::Editor;
 
 // main function
 fn main() {
-    let editor = Editor::default();
-    editor.run();
+    Editor::default().run();
 }
 
