12b143680336fbd1d2b65dbb31af268b4e914c65
sohaib farsi <ssohaibfarsi@pm.me>
2023-07-03 20:27:23 +0200

    refactored the code and implemented a 'class' with sub funcs instead of writing everything in the main file, also added clippy shbang to help during compiling

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,10 @@
-// imports the necesssary crates
-use std::io::{self, stdout};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-
-fn die(e: std::io::Error) {
-    // prints the error
-    panic!("{}", e);
-}
+#![warn(clippy::all, clippy::pedantic)]
+mod editor;
+use editor::Editor;
 
 // main function
 fn main() {
-    // creates a raw terminal
-    let _stdout = stdout().into_raw_mode().unwrap();
-    
-    for key in io::stdin().keys() {
-        // match the key
-        match key {
-            // if the key is Ok, then match the key
-            Ok(key) => match key {
-                // prints the key and the character
-                Key::Char(c) => {
-                    if c.is_control() {
-                        // if the key is a control key, then print the key
-                        println!("{:?}\r", c as u8);
-                    } else {
-                        // if the key is not a control key, then print the key and the character
-                        println!("{:?} ({})\r", c as u8, c);
-                    }
-                }
-
-                // if the key is Ctrl + q, then break
-                Key::Ctrl('q') => break,
-                // for any unhandled key, then print the key
-                _ => println!("{:?}\r", key),
-            }
-
-            Err(e) => die(e),
-        }
-    }
+    let editor = Editor::default();
+    editor.run();
 }
 
